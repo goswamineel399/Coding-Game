5f726f5ddd0ca69e401048754f15e78ef9e89e73
Surya Jasper <suryajasper2004@gmail.com>
2020-03-19 17:20:28 -0700

    made police officer models

---
+++ Coding Game/Assets/Scripts/introSequence.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using UnityEngine;
+using TMPro;
+
+public class introSequence : MonoBehaviour
+{
+    [TextArea(0, 30)]
+    public string [] messages;
+
+    public float duration;
+    public float waitBetweenMessages;
+
+    public TMP_Text textOut;
+
+    public bool disable;
+
+    private bool canStart;
+    private float timer;
+    private int currIndex;
+    private int globalIndex;
+
+    void Start()
+    {
+        globalIndex = 0;
+        currIndex = 1;
+        textOut.text = "";
+    }
+
+    void Update()
+    {
+        if (canStart)
+        {
+            if (Time.time - timer >= duration)
+            {
+                if (globalIndex <= messages.Length)
+                    textOut.text = messages[globalIndex].Substring(0, currIndex++);
+
+                if (currIndex >= messages[globalIndex].Length)
+                {
+                    currIndex = 0;
+                    globalIndex++;
+                }
+
+                //reset timer
+                timer = Time.time;
+            }
+        }
+    }
+
+    IEnumerator Wait(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+    }
+
+    public void startAnimation()
+    {
+        if (!disable)
+        {
+            timer = Time.time;
+            canStart = true;
+        }
+    }
+}
