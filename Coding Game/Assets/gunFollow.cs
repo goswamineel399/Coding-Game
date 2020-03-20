fd512137b9a6b89547da9ca945f522d34417a9fa
Surya Jasper <suryajasper2004@gmail.com>
2020-03-20 11:03:02 -0700

    Changed to humanoid rig

---
+++ Coding Game/Assets/gunFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gunFollow : MonoBehaviour
+{
+    public Transform toFollow;
+    public Vector3 offset;
+    public bool followRotation;
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = toFollow.position + offset;
+        if (followRotation)
+        {
+            transform.rotation = toFollow.rotation;
+        }
+    }
+}
